0c1965978cd142af23f4a5ecf8aac9fddd0726b0
smudesign <urwinsamantha@gmail.com>
2022-01-13 16:03:55 -0500

    graphql queries

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  // create new Apollo server and pass in schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  // start apollo server
+  await server.start();
+
+  // integrate apollo server with express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
