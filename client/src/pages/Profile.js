de03f1d1e6df6aaccfebf7401e0ad0b07e1b9fac
smudesign <urwinsamantha@gmail.com>
2022-01-16 19:08:26 -0500

    add friend and thought form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Redirect, useParams } from 'react-router-dom';
+
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
+
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Profile = () => {
@@ -13,6 +17,8 @@ const Profile = () => {
     variables: { username: userParam }
   });
   const user = data?.me || data?.user || {};
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to='/profile' />;
@@ -30,13 +36,30 @@ const Profile = () => {
     return <div>loading...</div>
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {userParam ? `${user.username}'s` : 'your'} profile.
+          {userParam ? `${user.username}'s` : 'Your'} profile.
         </h2>
+        {userParam && (
+          <button className='btn ml-auto' onClick={handleClick}>
+          Add Friend
+          </button>
+        )}
       </div>
+
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
